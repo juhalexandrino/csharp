3328d13a660bfae04215f21175c5445a4c233d33
juhalexandrino <juliasalexandrino@hotmail.com>
2023-08-29 16:10:26 -0300

    atualizando exercício 01 e 02 na pasta aula_06

--- aula_06/Exercícios/Estrutura de Dados/Exercicio01/Program.cs
+++ aula_06/Exercícios/Estrutura de Dados/Exercicio01/Program.cs
@@ -1,4 +1,6 @@
-﻿namespace Exercicio01
+﻿using System.Collections.Generic;
+
+namespace Exercicio01
 {
     internal class Program
     {
@@ -8,26 +10,61 @@
             string? nomeCliente;
             int entrada;
 
-            Console.WriteLine("------------------------------------------------");
-            Console.WriteLine("1 - Adicionar cliente na fila");
-            Console.WriteLine("2 - Listar todos os clientes");
-            Console.WriteLine("3 - Retirar cliente da fila");
-            Console.WriteLine("0 - Sair");
-            Console.WriteLine("------------------------------------------------");
+            do{
+                Console.WriteLine("===============================================");
+                Console.WriteLine("       1 - Adicionar cliente na fila");
+                Console.WriteLine("       2 - Listar todos os clientes");
+                Console.WriteLine("       3 - Retirar cliente da fila");
+                Console.WriteLine("       0 - Sair");
+                Console.WriteLine("===============================================");
 
-            Console.WriteLine("Escolha a opção desejada: ");
-            entrada = Convert.ToInt32(Console.ReadLine());
+                Console.Write("\nEscolha a opção desejada: ");
+                entrada = Convert.ToInt32(Console.ReadLine());
 
-            switch (entrada){
-                case 1:
-                    Console.WriteLine("Digite o nome: ");
-                    nomeCliente = Console.ReadLine();
-                    cliente.Enqueue(nomeCliente);
-                    Console.WriteLine("Fila atual: ");
-                    Console.Write(nomeCliente);
-                    Console.WriteLine("Cliente adicionado!");
+                if (entrada == 0)
+                    Console.WriteLine("Programa finalizado!");
+                else if (entrada < 0 || entrada >= 4)
+                    Console.WriteLine("Digite uma opção válida.");
+                
+                switch (entrada){
+                   
+                    case 1:
+                        Console.Write("Digite o nome: ");
+                        nomeCliente = Console.ReadLine();
+                        cliente.Enqueue(nomeCliente);
+                        
+                        Console.WriteLine("\nFila atual: ");
+                        foreach (var todosClientes in cliente){
+                            
+                            Console.WriteLine(todosClientes);
+                        }
+                        Console.Write("\n\nCliente adicionado com sucesso!\n");
                     break;
-            }
+
+                    case 2:
+                        foreach (var todosClientes in cliente){
+                        Console.WriteLine("" + todosClientes);
+                        }
+
+                        if (cliente.Count == 0)
+                            Console.WriteLine("A fila está vazia!");
+                    break;
+
+                    case 3:
+                        
+                        cliente.Dequeue();
+                        foreach (var todosClientes in cliente){
+                            Console.WriteLine("" + todosClientes);
+                        }
+
+                        Console.WriteLine("Cliente foi chamado!");
+
+                        if (cliente.Count == 0)
+                            Console.WriteLine("A fila está vazia!");
+                    break;
+                }
+
+            }while (entrada > 0 && entrada <=3);            
         }
     }
 }
---
+++ aula_06/Exercícios/Estrutura de Dados/Exercicio02/Program.cs
@@ -0,0 +1,66 @@
+﻿namespace Exercicio02
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Stack<string> livro = new Stack<string>();
+            string? nomeLivro;
+            int entrada;
+
+            do{
+                Console.WriteLine("===============================================");
+                Console.WriteLine("       1 - Adicionar livro na pilha");
+                Console.WriteLine("       2 - Listar todos os livros");
+                Console.WriteLine("       3 - Retirar livro da pilha");
+                Console.WriteLine("       0 - Sair");
+                Console.WriteLine("===============================================");
+
+                Console.Write("\nEscolha a opção desejada: ");
+                entrada = Convert.ToInt32(Console.ReadLine());
+
+                if (entrada == 0)
+                    Console.WriteLine("Programa finalizado!");
+                else if (entrada < 0 || entrada >= 4)
+                    Console.WriteLine("Digite uma opção válida.");
+
+                switch (entrada){
+
+                    case 1:
+                        Console.Write("Digite o nome: ");
+                        nomeLivro = Console.ReadLine();
+                        livro.Push(nomeLivro);
+
+                        Console.WriteLine("\nFila atual: ");
+                        foreach (var todosLivros in livro){
+                            Console.WriteLine(todosLivros);
+                        }
+                        Console.Write("\n\nLivro adicionado com sucesso!\n");
+                        break;
+
+                    case 2:
+                        foreach (var todosLivros in livro){
+                            Console.WriteLine(todosLivros);
+                        }
+
+                        if (livro.Count == 0)
+                            Console.WriteLine("\nA fila está vazia!");
+                        break;
+
+                    case 3:
+                        livro.Pop();
+                        foreach (var todosLivros in livro){
+                            Console.WriteLine("" + todosLivros);
+                        }
+
+                        Console.WriteLine("Um livro foi retirado da pilha!");
+
+                        if (livro.Count == 0)
+                            Console.WriteLine("A fila está vazia!");
+                    break;
+                }
+
+            } while (entrada > 0 && entrada <= 3);
+        }
+    }
+}
